f0a4aed58e3f03085eb556bd841385ddc170cd1e
=skush554@gmail.com <skushwaha9425@gmail.com>
2024-10-03 23:22:24 +0530

    second commit

---
+++ src/IT_Coaching/GithubDeletekrdotesting.java
@@ -0,0 +1,10 @@
+package IT_Coaching;
+
+public class GithubDeletekrdotesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("sk");
+	}
+
+}
